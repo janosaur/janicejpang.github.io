0557dd04124767039dbcefb10ea25d91ed42d881
janosaur <janice.jamye.pang@gmail.com>
2015-04-21 14:32:53 -0700

    Update pocket.js

--- portal/js/pocket.js
+++ portal/js/pocket.js
@@ -23,6 +23,11 @@ $(document).ready(function() {
             $('.wealthDetail').slideUp();
             $('.knowledgeDetail').slideUp();
             $('.items .music').css('background-color', '#d5d0af');
+            $('.items .light').css('background-color', '#fff9e7');
+            $('.items .memory').css('background-color', '#fff9e7');
+            $('.items .protection').css('background-color', '#fff9e7');
+            $('.items .wealth').css('background-color', '#fff9e7');
+            $('.items .knowledge').css('background-color', '#fff9e7');
             $('.musicDetail').css('background-image', 'url(https://cloud.githubusercontent.com/assets/7544281/7255651/75722f8e-e7ff-11e4-9b7f-e3988652d027.png)');
             $('.musicDetail').slideDown();
         } else {
@@ -40,11 +45,16 @@ $(document).ready(function() {
             $('.protectionDetail').slideUp();
             $('.wealthDetail').slideUp();
             $('.knowledgeDetail').slideUp();
-                        $('.items .light').css('background-color', '#d5d0af');
+            $('.items .light').css('background-color', '#d5d0af');
+            $('.items .music').css('background-color', '#fff9e7');
+            $('.items .memory').css('background-color', '#fff9e7');
+            $('.items .protection').css('background-color', '#fff9e7');
+            $('.items .wealth').css('background-color', '#fff9e7');
+            $('.items .knowledge').css('background-color', '#fff9e7');
             $('.lightDetail').css('background-image', 'url(https://cloud.githubusercontent.com/assets/7544281/7255650/756f9878-e7ff-11e4-80b5-c8b83ae5536b.png)');
             $('.lightDetail').slideDown();
         } else {
-                        $('.items .light').css('background-color', '#fff9e7');
+            $('.items .light').css('background-color', '#fff9e7');
             $('.lightDetail').slideUp();
         }
     }); // end LIGHT click
@@ -59,10 +69,15 @@ $(document).ready(function() {
             $('.wealthDetail').slideUp();
             $('.knowledgeDetail').slideUp();
             $('.items .memory').css('background-color', '#d5d0af');
+            $('.items .music').css('background-color', '#fff9e7');
+            $('.items .light').css('background-color', '#fff9e7');
+            $('.items .protection').css('background-color', '#fff9e7');
+            $('.items .wealth').css('background-color', '#fff9e7');
+            $('.items .knowledge').css('background-color', '#fff9e7');
             $('.memoryDetail').css('background-image', 'url(https://cloud.githubusercontent.com/assets/7544281/7255656/7583ea12-e7ff-11e4-9812-ee3ce6e119c3.png)');
             $('.memoryDetail').slideDown();
         } else {
-                        $('.items .memory').css('background-color', '#fff9e7');
+            $('.items .memory').css('background-color', '#fff9e7');
             $('.memoryDetail').slideUp();
         }
     }); // end MEMORY click
@@ -77,10 +92,15 @@ $(document).ready(function() {
             $('.wealthDetail').slideUp();
             $('.knowledgeDetail').slideUp();
             $('.items .protection').css('background-color', '#d5d0af');
+            $('.items .music').css('background-color', '#fff9e7');
+            $('.items .light').css('background-color', '#fff9e7');
+            $('.items .memory').css('background-color', '#fff9e7');
+            $('.items .wealth').css('background-color', '#fff9e7');
+            $('.items .knowledge').css('background-color', '#fff9e7');
             $('.protectionDetail').css('background-image', 'url(https://cloud.githubusercontent.com/assets/7544281/7255646/756eb750-e7ff-11e4-9cd9-63882c4208cb.png)');
             $('.protectionDetail').slideDown();
         } else {
-                        $('.items .protection').css('background-color', '#fff9e7');
+            $('.items .protection').css('background-color', '#fff9e7');
             $('.protectionDetail').slideUp();
         }
     }); // end PROTECTION click
@@ -95,10 +115,15 @@ $(document).ready(function() {
             $('.protectionDetail').slideUp();
             $('.knowledgeDetail').slideUp();
             $('.items .wealth').css('background-color', '#d5d0af');
+            $('.items .music').css('background-color', '#fff9e7');
+            $('.items .light').css('background-color', '#fff9e7');
+            $('.items .memory').css('background-color', '#fff9e7');
+            $('.items .protection').css('background-color', '#fff9e7');
+            $('.items .knowledge').css('background-color', '#fff9e7');
             $('.wealthDetail').css('background-image', 'url(https://cloud.githubusercontent.com/assets/7544281/7255653/758325b4-e7ff-11e4-9daf-564dcae9d025.png)');
             $('.wealthDetail').slideDown();
         } else {
-                        $('.items .wealth').css('background-color', '#fff9e7');
+            $('.items .wealth').css('background-color', '#fff9e7');
             $('.wealthDetail').slideUp();
         }
     }); // end WEALTH click
@@ -113,10 +138,15 @@ $(document).ready(function() {
             $('.protectionDetail').slideUp();
             $('.wealthDetail').slideUp();
             $('.items .knowledge').css('background-color', '#d5d0af');
+            $('.items .music').css('background-color', '#fff9e7');
+            $('.items .light').css('background-color', '#fff9e7');
+            $('.items .memory').css('background-color', '#fff9e7');
+            $('.items .protection').css('background-color', '#fff9e7');
+            $('.items .wealth').css('background-color', '#fff9e7');
             $('.knowledgeDetail').css('background-image', 'url(https://cloud.githubusercontent.com/assets/7544281/7255647/756f3dec-e7ff-11e4-97ab-ea4ddcc638e1.png)');
             $('.knowledgeDetail').slideDown();
         } else {
-                        $('.items .knowledge').css('background-color', '#fff9e7');
+            $('.items .knowledge').css('background-color', '#fff9e7');
             $('.knowledgeDetail').slideUp();
         }
     }); // end KNOWLEDGE click
